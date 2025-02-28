dde0d7ad54f397f921ee1be170b78764f70183b7
miah <131461350+lolxd8281989282@users.noreply.github.com>
2025-02-28 03:29:44 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -38,7 +38,6 @@ local tabs = {
 -- Legit Tab
 local Main = tabs.Legit:AddSection("Main", 1)
 local Visuals = tabs.Legit:AddSection("Visuals", 2)
-local Misc = tabs.Legit:AddSection("Misc", 2)
 
 -- Main Section
 Main:AddToggle({text = "Enabled", flag = "main_enabled"})
@@ -60,40 +59,29 @@ Main:AddList({text = "Checks", flag = "main_checks", values = {"..."}, value = "
 
 Main:AddSlider({text = "Field of View Radius", flag = "main_fov_radius", min = 0, max = 800, value = 80, suffix = "%/800%"})
 Main:AddSlider({text = "Alerts Duration", flag = "main_alerts_duration", min = 0, max = 10, value = 4, suffix = "%/10%"})
-Main:AddSlider({text = "Smoothing Amount", flag = "main_smoothing_amount", min = 0, max = 100, value = 15, suffix = "%/7%"})
+Main:AddSlider({text = "Smoothing Amount", flag = "main_smoothing_amount", min = 0, max = 7, value = 15, suffix = "%/7%"})
 
 -- Visuals Section
 Visuals:AddToggle({text = "Enabled", flag = "visuals_enabled"})
 Visuals:AddToggle({text = "Tracer", flag = "visuals_tracer"})
-Visuals:AddToggle({text = "Circle", flag = "visuals_circle"})
+Visuals:AddToggle({text = "Auto Select", flag = "visuals_auto_select"})
+Visuals:AddToggle({text = "Resolver", flag = "visuals_resolver"})
 Visuals:AddToggle({text = "Dot", flag = "visuals_dot"})
-Visuals:AddToggle({text = "Dot Filled", flag = "visuals_dot_filled"})
-Visuals:AddToggle({text = "Chams", flag = "visuals_chams"})
+Visuals:AddToggle({text = "Jump Offset (AIR)", flag = "visuals_jump_offset_air"})
+Visuals:AddToggle({text = "Spectate", flag = "visuals_spectate"})
+Visuals:AddToggle({text = "Air", flag = "visuals_air"})
+Visuals:AddToggle({text = "Anti Groundshots (AIR)", flag = "visuals_anti_groundshots_air"})
 
-Visuals:AddList({text = "Part", flag = "visuals_part", values = {"..."}, value = "..."})
-
-Visuals:AddSlider({text = "Thickness", flag = "visuals_thickness", min = 0, max = 10, value = 2, suffix = "%/10%"})
-Visuals:AddSlider({text = "Outline Transparency", flag = "visuals_outline_transparency", min = 0, max = 100, value = 5, suffix = "%/1%"})
-Visuals:AddSlider({text = "Fill Transparency", flag = "visuals_fill_transparency", min = 0, max = 100, value = 5, suffix = "%/1%"})
+Visuals:AddSlider({text = "Refresh Rate", flag = "visuals_refresh_rate", min = 0, max = 200, value = 200, suffix = "ms/200ms"})
+Visuals:AddSlider({text = "To Take Off", flag = "visuals_to_take_off", min = 0, max = 1, value = 0.4, suffix = "'/1'"})
+Visuals:AddSlider({text = "Jump Offset", flag = "visuals_jump_offset", min = 0, max = 4, value = 0.09, suffix = "'/4'"})
 Visuals:AddSlider({text = "Dot Size", flag = "visuals_dot_size", min = 0, max = 10, value = 6, suffix = "%/10%"})
-Visuals:AddSlider({text = "Part Transparency", flag = "visuals_part_transparency", min = 0, max = 100, value = 5, suffix = "%/1%"})
+Visuals:AddSlider({text = "Part Transparency", flag = "visuals_part_transparency", min = 0, max = 1, value = 0.05, suffix = "%/1%"})
 Visuals:AddSlider({text = "Part Size", flag = "visuals_part_size", min = 0, max = 20, value = 2, suffix = "'/20'"})
 Visuals:AddSlider({text = "Circle Size", flag = "visuals_circle_size", min = 0, max = 10, value = 2, suffix = "%/10%"})
 
 Visuals:AddToggle({text = "Part Material", flag = "visuals_part_material"})
 Visuals:AddToggle({text = "Neon", flag = "visuals_neon"})
-
--- Misc Section
-Misc:AddToggle({text = "Auto Select", flag = "misc_auto_select"})
-Misc:AddToggle({text = "Resolver", flag = "misc_resolver"})
-Misc:AddToggle({text = "Jump Offset (AIR)", flag = "misc_jump_offset_air"})
-Misc:AddToggle({text = "Spectate", flag = "misc_spectate"})
-Misc:AddToggle({text = "Air", flag = "misc_air"})
-Misc:AddToggle({text = "Anti Groundshots (AIR)", flag = "misc_anti_groundshots_air"})
-
-Misc:AddSlider({text = "Refresh Rate", flag = "misc_refresh_rate", min = 0, max = 200, value = 200, suffix = "ms/200ms"})
-Misc:AddSlider({text = "To Take Off", flag = "misc_to_take_off", min = 0, max = 10, value = 0.4, suffix = "'/1'"})
-Misc:AddSlider({text = "Jump Offset", flag = "misc_jump_offset", min = 0, max = 4, value = 0.09, suffix = "'/4'"})
 
 -- Finalize
 library:SendNotification("Successfully Initialized", 3, Color3.new(0, 1, 0))
