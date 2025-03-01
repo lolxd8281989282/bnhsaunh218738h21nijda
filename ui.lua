7f3f4305f5661052d1fea6a5c3f567a90d501080
miah <131461350+lolxd8281989282@users.noreply.github.com>
2025-03-01 05:16:01 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -3,8 +3,8 @@ return function(Window, Library, ESP)
     local Visuals = Window:Page({Name = "visuals"})
     local ESPSection = Visuals:Section({Name = "ESP", Side = "Left"})
 
-    -- Add ESP toggles
-    ESPSection:Toggle({Name = "Enabled", Default = ESP.Settings.Enabled, Pointer = "ESPEnabled"})
+    -- Add ESP toggles with correct pointers matching ESP.Settings keys
+    ESPSection:Toggle({Name = "Enabled", Default = ESP.Settings.Enabled, Pointer = "Enabled"})
     ESPSection:Toggle({Name = "Show Names", Default = ESP.Settings.ShowNames, Pointer = "ShowNames"})
     ESPSection:Toggle({Name = "Show Boxes", Default = ESP.Settings.ShowBoxes, Pointer = "ShowBoxes"})
     ESPSection:Toggle({Name = "Show Health Bars", Default = ESP.Settings.ShowHealthBars, Pointer = "ShowHealthBars"})
@@ -16,18 +16,26 @@ return function(Window, Library, ESP)
     ESPSection:Colorpicker({Name = "Health Bar Color", Info = "ESP Health Bar Color", Default = ESP.Settings.HealthBarColor, Pointer = "HealthBarColor"})
 
     -- Update ESP settings when UI changes
-    for _, pointer in pairs(Library.pointers) do
+    for name, pointer in pairs(Library.pointers) do
         if type(pointer) == "table" and type(pointer.Set) == "function" then
             local originalSet = pointer.Set
             pointer.Set = function(self, value)
                 originalSet(self, value)
-                -- Update ESP settings
-                local setting = pointer.flag
-                if ESP.Settings[setting] ~= nil then
-                    ESP.Settings[setting] = value
+                -- Update ESP settings using the pointer name
+                if ESP.Settings[name] ~= nil then
+                    ESP.Settings[name] = value
+                    -- Force ESP update when settings change
+                    ESP:UpdateESP()
                 end
                 return value
             end
+        end
+    end
+
+    -- Initial settings update
+    for name, pointer in pairs(Library.pointers) do
+        if ESP.Settings[name] ~= nil and type(pointer.Get) == "function" then
+            ESP.Settings[name] = pointer:Get()
         end
     end
 
